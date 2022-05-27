a3cfe01b04fb3b107e0128e8b36ed8b36ef99602
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2022-05-27 14:52:42 +0300

    finish

--- app.py
+++ app.py
@@ -1,45 +1,36 @@
 from flask import Flask, render_template, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from helpers.dynamodb import DynamoDB
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
-
-
-class Todo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    complated = db.Column(db.Integer, default=0)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
 
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        table = DynamoDB()
 
         try:
-            db.session.add(new_task)
-            db.session.commit()
+            table.addItem(task_content)
             return redirect('/')
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        table = DynamoDB()
+        tasks = table.getListItem()
+        if len(tasks) > 1:
+            tasks = sorted(
+                tasks,
+                key=lambda x: datetime.strptime(x['date_created'], "%d-%b-%Y (%H:%M:%S)"), reverse=False
+            )
         return render_template('index.html', tasks=tasks)
 
 
 @app.route("/delete/<int:id>")
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
     try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
+        DynamoDB().delteItem(id)
         return redirect('/')
     except:
         return 'There was a problem deleting that task'
@@ -47,13 +38,13 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    task = DynamoDB().getItem(id)
 
     if request.method == 'POST':
-        task.content = request.form['content']
+        content = request.form['content']
 
         try:
-            db.session.commit()
+            DynamoDB().updateItem(id, content)
             return redirect('/')
         except:
             return 'There was an issue updating your task'
---
+++ helpers/dynamodb.py
@@ -0,0 +1,92 @@
+import boto3
+from datetime import datetime
+from botocore.exceptions import ClientError
+import logging
+
+logger = logging.getLogger(__name__)
+table_name = "todo_list"
+
+
+class DynamoDB():
+    def __init__(self):
+        session = boto3.Session(
+            aws_access_key_id="AKIAX7MFXFJLFOAQRYN5",
+            aws_secret_access_key="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",
+        )
+
+        client = session.resource('dynamodb')
+        self.table = client.Table(table_name)
+
+    def exists(self):
+        print(self.table.creation_date_time)
+
+    def getListItem(self):
+        response = self.table.scan()
+        items = response['Items']
+        return items
+        
+
+    def addItem(self, content):
+        try:
+            id = self.getListItem()
+            dateTimeObj = datetime.now()
+            self.table.put_item(
+                Item={
+                    'id': str(len(id)),
+                    'content': content,
+                    'date_created': dateTimeObj.strftime("%d-%b-%Y (%H:%M:%S)"),
+                }
+            )
+        except ClientError as err:
+            logger.error(
+                "Couldn't add data to table %s. Here's why: %s: %s", table_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+    def getItem(self, id):
+        try:
+            response = self.table.get_item(
+                Key={
+                    'id': str(id),
+                }
+            )
+
+        except ClientError as err:
+            logger.error(
+                "Couldn't fetch data from table %s. Here's why: %s: %s", table_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+        else:
+            item = response['Item']
+            print(item)
+            return item
+
+    def delteItem(self, id):
+        try:
+            self.table.delete_item(
+                Key={
+                    'id': str(id),
+                }
+            )
+        except ClientError as err:
+            logger.error(
+                "Couldn't delete data from table %s. Here's why: %s: %s", table_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
+
+    def updateItem(self, id, content):
+        try:
+            self.table.update_item(
+                Key={
+                    'id': str(id),
+                },
+                UpdateExpression='SET content = :val1',
+                ExpressionAttributeValues={
+                    ':val1': content
+                }
+            )
+        except ClientError as err:
+            logger.error(
+                "Couldn't update data for table %s. Here's why: %s: %s", table_name,
+                err.response['Error']['Code'], err.response['Error']['Message'])
+            raise
